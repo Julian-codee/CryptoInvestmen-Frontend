4c9c71409a51c4f058d3420af3f2b1e604dae2b6
Julian_codee <mmm808383@gmail.com>
2026-02-18 17:52:38 -0500

    Realizamos el apartado del portafolio al respecto con la visual al respecto

--- src/Pages/Portfolio.jsx
+++ src/Pages/Portfolio.jsx
@@ -1,9 +1,26 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import { getPortfolio } from '../Services/api';
+import CryptoCard from '../Components/CryptoCard';
 
 export default function Portfolio() {
+
+  const [data, setData] = useState({});
+
+  useEffect(() => {
+    fetchData();
+  }, []);
+
+  const fetchData = async () => {
+    const response = await getPortfolio();
+    setData(response.data);
+  };
+
   return (
-    <div>
-      
+    <div className="p-5 flex gap-5">
+      {Object.keys(data).map((symbol) => {
+        const coin = data[symbol][0];
+        return <CryptoCard key={symbol} coin={coin} />;
+      })}
     </div>
   )
 }
