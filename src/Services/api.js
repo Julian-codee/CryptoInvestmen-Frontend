37b53b9294968f5fd71276c85973bab6327c706e
Julian_codee <mmm808383@gmail.com>
2026-02-18 16:27:01 -0500

    Realizamos la carpeta de Service en la cual creamos el archivo de api.js la cual nos ayudara proximamente a la conexion del backend

---
+++ src/Services/api.js
@@ -0,0 +1,10 @@
+import axios from "axios";
+
+const API = axios.create({
+    baseURL: "http://localhost:3000",
+});
+
+export const getPortfolio = () =>. API.get("/portfolio");
+export const searchCryptos = (query) => API.get(`/search?q=${query}`);
+export const getHistorical = (symbol, start, end) => 
+    API.get(`/historical/${symbol}?start=${start}&end=${end}`);
