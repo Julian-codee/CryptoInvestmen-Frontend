e5c3053cda28e57dd8e088189de65dbcb5e28de8
Julian_codee <mmm808383@gmail.com>
2026-02-18 17:17:50 -0500

    Realizamos los primeros campos de la tabla para la visual del sistema

--- src/Components/MarketTable.jsx
+++ src/Components/MarketTable.jsx
@@ -3,12 +3,15 @@ import React from 'react'
 export default function MarketTable() {
     return (
         <div className="overflow-x-auto"> {/* Contenedor para que sea responsive en móviles */}
-            <table>
+            <table className="w-full mt-5 border-collapse text-left text-sm md:text-base">
                 <thead>
-                    <tr>
-                        <th>
-
-                        </th>
+                    <tr className="text-gray-500 border-b border-gray-800 uppercase text-xs tracking-wider">
+                        <th className="py-3 px-4">Nombre</th>
+                        <th className="py-3 px-4 text-right">Precio</th>
+                        <th className="py-3 px-4 text-right">1h</th>
+                        <th className="py-3 px-4 text-right">24h</th>
+                        <th className="py-3 px-4 text-right">7d</th>
+                        <th className="py-3 px-4 text-right">Cap</th>
                     </tr>
                 </thead>
                 <tbody>
