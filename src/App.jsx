50611b7e15018e95ce7400fa361c8efec706a368
Julian_codee <mmm808383@gmail.com>
2026-02-18 16:37:43 -0500

    realizamos react router Dom , para las rutas que manejaremos dentro del proyecto para su utilidad al respecto

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,23 @@
-import { useState } from 'react'
-import './App.css'
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Header from "./Components/Header";
+import Market from "./Pages/Market";
+import Charts from "./Pages/Charts";
+import Portfolio from "./Pages/Portfolio";
+import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
     <>
-    
+      <Router>
+        <Header />
+        <Routes>
+          <Route path="/" element={Market} />
+          <Route path="/charts" element={Charts} />
+          <Route path="/portfolio" element={Portfolio} />
+        </Routes>
+      </Router>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
