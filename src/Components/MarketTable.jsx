85962a1ca3d58011c31f8c9cbd945f05daa0db9e
Julian_codee <mmm808383@gmail.com>
2026-02-18 17:05:50 -0500

    Realizamos cambios ya que habiamos puesto parte de la logica dentro de markettable lo cual era incorrecto la alojamos en market y preparamos market table para la siguiente parte del codigo, dentro de market realizamos una asincronia de peticion de datos

--- src/Components/MarketTable.jsx
+++ src/Components/MarketTable.jsx
@@ -1,9 +1,26 @@
-import React from 'react'
+import React, { useEffect } from 'react'
+import { getPortfolio } from '../Services/api';
+import MarketTable from '../Components/MarketTable';
 
 export default function MarketTable() {
-  return (
-    <div>
-      
-    </div>
-  )
+    // Uso de estados
+    const [data, setData] = useState({});
+
+    // disparador
+    useEffect(() => {
+        fetchData();
+    }, []);
+
+    // llamado a la API para traer los datos del portafolio
+    const fetchData = async () => {
+        const response = await getPortfolio();
+        setData(response.data);
+    }
+
+    return (
+        <div className='p-10'>
+            <h2>Mercado</h2>
+            <MarketTable />
+        </div>
+    )
 }
